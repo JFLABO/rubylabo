7fe6dabece799d0940135e89045cde69e44161d0
JFLABO <vortex003@msn.com>
2019-08-23 21:39:27 +0900

    Many people make money on this job

--- 201908/rb/j.rb
+++ 201908/rb/j.rb
@@ -1,4 +1,4 @@
 require 'json'
-hash = { "Ocean" => { "Squid" => 10, "Octopus" =>8 }}
+hash = { "Ocean" => { "Squid" => 10, "Octopus" =>8 },"mes"=>{"lucky"=>"Many people make money on this job.(C)JFLABO 2019-"}}
 json_str = JSON.pretty_generate(hash)
 puts json_str
